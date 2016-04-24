30b1973c1753bfe6a7e5b70281f3fad59420a902
Will Read <will.read@gmail.com>
2016-04-24 12:53:44 -0700

    New Plans
    omniscopeio/huginn#27

---
+++ db/migrate/20160424063250_update_plans.rb
@@ -0,0 +1,27 @@
+class UpdatePlans < ActiveRecord::Migration
+  def up
+    Plan.where.not(stripe_id: 'hobby-plan').destroy_all
+
+    Plan.create do |plan|
+      plan.name = 'Hobby Standard 7'
+      plan.stripe_id = 'hobby-standard-7'
+      plan.price = 7.0
+      plan.interval = 'month'
+      plan.features = "Unlimited Agents\n\nUnlimited Scenarios\n\nUnlimited Events"
+      plan.highlight = false
+      plan.display_order = 1
+    end
+
+    Plan.create do |plan|
+      plan.name = 'Hobby Fast 11'
+      plan.stripe_id = 'hobby-fast-11'
+      plan.price = 11.0
+      plan.interval = 'month'
+      plan.features = "Unlimited Agents\n\nUnlimited Scenarios\n\nUnlimited Events\n\nPropagate Events Immediately"
+      plan.highlight = false
+      plan.display_order = 2
+    end
+  end
+
+  class Plan < ActiveRecord::Base; end
+end
