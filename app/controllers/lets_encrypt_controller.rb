fe2db621d20858f25c1f9dae8bf50dbb920883f8
Will Read <will.read@gmail.com>
2016-10-30 21:45:37 -0500

    New key for subdomain

--- app/controllers/lets_encrypt_controller.rb
+++ app/controllers/lets_encrypt_controller.rb
@@ -1,5 +1,5 @@
 class LetsEncryptController < ApplicationController
   def show
-    render text: "iGxM9fCd6QSPEej8a_owyRZ2elKqbrLSQ7SZb4P6OlI.guKMswe7OQBxH5HxwtXY_43DUM5x2iq3_ZqjvwT7txg"
+    render text: "pvEuSKM0mI4tSu5L2avJUCcXlYCcV36XyWEkCV3HB0M.guKMswe7OQBxH5HxwtXY_43DUM5x2iq3_ZqjvwT7txg"
   end
 end
