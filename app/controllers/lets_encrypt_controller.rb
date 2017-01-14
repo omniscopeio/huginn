e28b53e0b8d835809d3eab88c586decc8917aec0
Will Read <will.read@gmail.com>
2017-01-14 11:03:49 -0800

    Renewing SSL cert

--- app/controllers/lets_encrypt_controller.rb
+++ app/controllers/lets_encrypt_controller.rb
@@ -1,5 +1,5 @@
 class LetsEncryptController < ApplicationController
   def show
-    render text: "h56_XpvZCh-PjluEmOKzpb4E8LvywSX2NTeZgeCD8Ug.guKMswe7OQBxH5HxwtXY_43DUM5x2iq3_ZqjvwT7txg"
+    render text: "OQqv33KlcBQsQPtmLx6d8XdxeBAGcBrJsOpHuRQAjwA.guKMswe7OQBxH5HxwtXY_43DUM5x2iq3_ZqjvwT7txg"
   end
 end
